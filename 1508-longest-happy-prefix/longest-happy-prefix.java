7edef02e1ff4b8b397dfd1cba1338354cfa563af
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-06-30 14:34:37 +0530

    Time: 6 ms (96.60%) | Memory: 45.8 MB (22.58%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.java
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -1,12 +1,12 @@
 class Solution {
     public String longestPrefix(String s) {
         int[] lsp = new int[s.length()];
-
+        char[] str = s.toCharArray();
         int len=0;
         int i = 1;
 
-        while(i<s.length()){
-            if(s.charAt(i) == s.charAt(len)){
+        while(i<str.length){
+            if(str[i] == str[len]){
                 len++;
                 lsp[i] = len;
                 i++;
