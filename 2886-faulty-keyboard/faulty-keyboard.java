908504b798f5a17c6e36c2728f33914db2974526
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-07-02 09:43:23 +0530

    Time: 3 ms (95.19%) | Memory: 44.8 MB (60.00%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public String finalString(String s) {
+        StringBuilder str = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(ch =='i'){
+                str.reverse();
+            }
+            else{
+                str.append(ch);
+            }
+        }
+        return str.toString();
+    }
+}
