d384701bb95d4d246a0f7cd6c01ffc27e54f4143
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-07-10 14:40:17 +0530

    Time: 2 ms (94.54%) | Memory: 42.6 MB (31.82%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] count = new int[26];     // frequency of each char
+        boolean[] inStack = new boolean[26]; // to check if a char is in stack
+        Stack<Character> stk = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++; // count occurrences
+        }
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']--; // one less occurrence left
+
+            if (inStack[c - 'a']) continue;
+
+            // pop characters that are greater than current and still have remaining occurrences
+            while (!stk.isEmpty() && stk.peek() > c && count[stk.peek() - 'a'] > 0) {
+                inStack[stk.pop() - 'a'] = false;
+            }
+
+            stk.push(c);
+            inStack[c - 'a'] = true;
+        }
+
+        // build result
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stk) {
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+}
