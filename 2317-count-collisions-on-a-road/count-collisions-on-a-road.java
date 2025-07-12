b8839bf7231eda1b61c5165beeee7a0066608e20
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-07-12 12:43:22 +0530

    Time: 9 ms (94.69%) | Memory: 45.3 MB (93.47%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,45 +1,27 @@
 class Solution {
     public int countCollisions(String directions) {
-        Stack<Character> stk = new Stack<>();
+       char[] car = directions.toCharArray();
         int cnt = 0;
-        
-        for(char ch : directions.toCharArray()){
-            if(ch == 'L'){
-                if(stk.isEmpty()){
-                    stk.push(ch); // no collision
-                } else if(stk.peek() == 'S'){
-                    cnt += 1; // L hits S
-                } else if(stk.peek() == 'R'){
-                    // pop all R’s and add collisions
-                    while(!stk.isEmpty() && stk.peek() == 'R'){
-                        stk.pop();
-                        cnt++;
-                    }
-                    // L also becomes S after collision
-                    cnt++;
-                    stk.push('S');
-                } else {
-                    stk.push(ch); // L after L
-                }
-            }
-            else if(ch == 'S'){
-                if(stk.isEmpty()){
-                    stk.push('S');
-                } else if(stk.peek() == 'R'){
-                    // pop all R’s and count collisions
-                    while(!stk.isEmpty() && stk.peek() == 'R'){
-                        stk.pop();
-                        cnt++;
-                    }
-                    stk.push('S');
-                } else {
-                    stk.push('S');
-                }
-            }
-            else{ // ch == 'R'
-                stk.push('R');
-            }
+       int left = 0;
+
+       while(left < car.length && car[left] == 'L'){
+        left++;
+       }
+
+       int right = car.length -1 ;
+
+       while(right >= 0 && car[right] == 'R'){
+        right--;
+       }
+
+
+       for(int i =left ; i<= right; i++){
+        if(car[i] != 'S'){
+            cnt++;
         }
+       }
+
         return cnt;
+
     }
 }
