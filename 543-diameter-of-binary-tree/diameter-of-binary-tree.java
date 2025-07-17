c4b6ed1a01ff1dc34b1e69bce5aec1d11eed0fa5
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-07-17 14:40:06 +0530

    Time: 1 ms (29.23%) | Memory: 44.9 MB (38.52%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -14,6 +14,19 @@
  * }
  */
 class Solution {
+    class Pair<K, V> {
+    public K first;
+    public V second;
+
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+    public Pair() {
+        this.first = null;
+        this.second = null;
+    }
+}
     int max = 0;
     int height(TreeNode node){
         if(node == null) return 0;
@@ -26,8 +39,30 @@ class Solution {
 
         return ans;
     }
+
+    Pair<Integer, Integer> diameter(TreeNode root){
+        if(root == null){
+           return new Pair<>(0,0);
+        }
+
+
+        Pair<Integer,Integer> left = diameter(root.left);
+        Pair<Integer,Integer> right = diameter(root.right);
+
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second;
+
+        Pair<Integer,Integer> ans = new Pair<>();
+        ans.first = Math.max(op1, Math.max(op2,op3));
+        ans.second = Math.max(left.second, right.second)+1;
+
+        return ans;
+
+        
+    }
     public int diameterOfBinaryTree(TreeNode root) {
-        height(root);
-        return max;
+        
+       return diameter(root).first;
     }
 }
