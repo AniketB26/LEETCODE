f0712c50c7016c6ccd9dff340e5ec7548e922a6b
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-08-02 10:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (24.36%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -2,23 +2,36 @@ class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
 
-        for(int i =0; i< numRows; i++){
-            List<Integer> ll = new ArrayList<>();
+        // for(int i =0; i< numRows; i++){
+        //     List<Integer> ll = new ArrayList<>();
            
-            for(int j =0; j <=i; j++){
+        //     for(int j =0; j <=i; j++){
                 
-               if(j == 0 || j == i){
-                ll.add(1);
-               }
-               else{
-                int val = list.get(i-1).get(j-1) + list.get(i-1).get(j);
-                ll.add(val);
-               }
+        //        if(j == 0 || j == i){
+        //         ll.add(1);
+        //        }
+        //        else{
+        //         int val = list.get(i-1).get(j-1) + list.get(i-1).get(j);
+        //         ll.add(val);
+        //        }
 
                 
+        //     }
+
+        //     list.add(ll);
+        // }
+
+
+        for(int n = 0; n<numRows; n++){
+            List<Integer> row = new ArrayList<>();
+            long val = 1;
+            for(int k = 0; k<=n ; k++){
+
+                row.add((int)val);
+                val = val*(n-k)/(k+1);
             }
 
-            list.add(ll);
+            list.add(row);
         }
 
         return list;
