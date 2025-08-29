d4ab4710eac40a236cfa8e0c83a8392d45c663c9
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-08-29 18:13:27 +0530

    Time: 120 ms (7.35%) | Memory: 58.7 MB (11.64%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,71 +1,72 @@
 class Solution {
+    public static void nextSmallest(int[] h, int[] arr){
+        Stack<Integer> stk = new Stack<>();
+
+
+        for(int i =0; i<h.length; i++){
+
+            if(stk.isEmpty()){
+                stk.add(i);
+            }
+            else{
+                while(!stk.isEmpty() && h[stk.peek()] > h[i]){
+                    int idx = stk.pop();
+                    arr[idx] = i;
+                }
+                stk.add(i);
+            }
+        }
+
+         while (!stk.isEmpty()) {
+            arr[stk.peek()] = h.length;
+            stk.pop();
+        }
+    }
+
+    public static void prevSmallest(int[] h, int[] arr){
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i = h.length-1; i>=0; i--){
+            if(stk.isEmpty()){
+                stk.add(i);
+            }
+            else{
+                while(!stk.isEmpty() && h[stk.peek()] > h[i]){
+                    int idx = stk.pop();
+                    arr[idx] = i;
+                }
+                stk.add(i);
+            }
+        }
+        while (!stk.isEmpty()) {
+            arr[stk.peek()] = -1;
+            stk.pop();
+        }
+    }
     public int largestRectangleArea(int[] heights) {
-        // int left = 0;
-        // int right = 0;
-        // int n = heights.length;
+
+
         
-        // int maxarea = 0;
-        // for(int i =0; i< n; i++){
-        //     int min = heights[i];
-        //     left =i;
-        //     right =i;
-
-        //     while(left > 0 && heights[left-1] >= min){
-        //         left--;
-        //     }
-
-        //     while(right < n-1 && heights[right+1] >= min){
-        //         right++;
-        //     }
-
-
-        //     int width = right - left +1;
-        //     int area = min * width;
-
-        //     maxarea = Math.max(maxarea, area);
-
-            
-        // }
-
-        // return maxarea;
 
 
         int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int[] next = new int[n];
+        int[] prev = new int[n];
 
+        nextSmallest(heights, next);
+        prevSmallest(heights, prev);
+        
 
-        Stack<Integer> stk = new Stack<>();
+        int area = -1;
 
-        for(int i =0; i<n ; i++){
-            
-            while(!stk.isEmpty() && heights[stk.peek()] >= heights[i]){
-                stk.pop();
-            }
-
-            left[i] = stk.isEmpty() ? -1:stk.peek();
-            stk.push(i);
-        }
-
-        stk.clear();
-
-        for(int i =n-1; i>=0; i--){
-
-            while(!stk.isEmpty() && heights[stk.peek()] >= heights[i]){
-                stk.pop();
-            }
-
-            right[i] = stk.isEmpty() ? n : stk.peek();
-            stk.push(i);
-        }
-        int max = 0;
         for(int i =0; i<n; i++){
-            int width = right[i] - left[i] -1;
-            int area = heights[i] * width;
+            int width = next[i] - prev[i] -1;
 
-            max = Math.max(max,area); 
+            int newarea = width * heights[i];
+
+            area = Math.max(area,newarea);
         }
 
-        return max;
+        return area;
     }
 }
