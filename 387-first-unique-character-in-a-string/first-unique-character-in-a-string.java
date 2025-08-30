5d61aaeea6ac07aa17b1dab4dbf16cf0c24e6f41
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-08-30 14:12:34 +0530

    Time: 3 ms (99.85%) | Memory: 45.2 MB (83.60%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,32 +1,49 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,int[]> map = new HashMap<>();
+    //     HashMap<Character,int[]> map = new HashMap<>();
+
+    //     char[] arr = s.toCharArray();
+
+    //     for(int i =0; i<arr.length; i++){
+    //         if(!map.containsKey(arr[i])){
+    //             map.put(arr[i], new int[]{1,i});
+    //         }
+    //         else{
+    //             map.get(arr[i])[0]++;
+    //         }
+    //     }
+
+        
+
+    //     int min = Integer.MAX_VALUE;
+        
+    //     for(char x : map.keySet()){
+    //         int freq = map.get(x)[0];
+    //         int val = map.get(x)[1];
+    //         if(freq == 1){
+    //             min = Math.min(min,val);
+    //         }
+            
+            
+    //     }
+
+    //    return (min == Integer.MAX_VALUE) ? -1 : min;
 
         char[] arr = s.toCharArray();
+        int n = arr.length;
+        int[] freq = new int[256];
 
-        for(int i =0; i<arr.length; i++){
-            if(!map.containsKey(arr[i])){
-                map.put(arr[i], new int[]{1,i});
-            }
-            else{
-                map.get(arr[i])[0]++;
-            }
+
+        for(int i =0; i<n; i++){
+            freq[arr[i]]++;
         }
 
-        
-
-        int min = Integer.MAX_VALUE;
-        
-        for(char x : map.keySet()){
-            int freq = map.get(x)[0];
-            int val = map.get(x)[1];
-            if(freq == 1){
-                min = Math.min(min,val);
-            }
-            
-            
+        for(int i =0; i<n; i++){
+            if(freq[arr[i]] == 1) return i;
         }
 
-       return (min == Integer.MAX_VALUE) ? -1 : min;
+
+        return -1;
+
     }
 }
