69ed9c6e70a83974bacc0c55b80f79d76149f4a8
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-09-06 20:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (33.67%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -14,48 +14,60 @@
  * }
  */
 class Solution {
-    class Pair<K, V> {
-        public K first;
-        public V second;
+    // class Pair<K, V> {
+    //     public K first;
+    //     public V second;
 
-        public Pair(K first, V second) {
-            this.first = first;
-            this.second = second;
-        }
-        public Pair() {
-            this.first = null;
-            this.second = null;
-        }
+    //     public Pair(K first, V second) {
+    //         this.first = first;
+    //         this.second = second;
+    //     }
+    //     public Pair() {
+    //         this.first = null;
+    //         this.second = null;
+    //     }
+    // }
+
+    // Pair<Boolean,Integer> isBal(TreeNode root){
+    //     if(root== null){
+    //         return new Pair<>(true,0);
+    //     }
+
+    //     Pair<Boolean,Integer> left = isBal(root.left);
+    //     Pair<Boolean,Integer> right = isBal(root.right);
+
+    //     boolean leftans = left.first;
+    //     boolean rightans = right.first;
+
+    //     boolean diff = Math.abs(left.second - right.second) <=1;
+
+    //     Pair<Boolean, Integer> ans = new Pair<>();
+    //     ans.second = Math.max(left.second,right.second)+1;
+
+    //     if(leftans && rightans && diff){
+    //         ans.first = true;
+
+    //     } 
+    //     else{
+    //         ans.first = false;
+    //     }
+
+    //     return ans
+    
+    private int withmaxhieght(TreeNode root){
+        if(root == null) return 0;
+
+        int lh = withmaxhieght(root.left);
+        int rh = withmaxhieght(root.right);
+
+        if(lh == -1 || rh == -1) return -1;
+        if( Math.abs(lh-rh) > 1) return -1;
+
+        return 1+ Math.max(lh,rh);
     }
-
-    Pair<Boolean,Integer> isBal(TreeNode root){
-        if(root== null){
-            return new Pair<>(true,0);
-        }
-
-        Pair<Boolean,Integer> left = isBal(root.left);
-        Pair<Boolean,Integer> right = isBal(root.right);
-
-        boolean leftans = left.first;
-        boolean rightans = right.first;
-
-        boolean diff = Math.abs(left.second - right.second) <=1;
-
-        Pair<Boolean, Integer> ans = new Pair<>();
-        ans.second = Math.max(left.second,right.second)+1;
-
-        if(leftans && rightans && diff){
-            ans.first = true;
-
-        } 
-        else{
-            ans.first = false;
-        }
-
-        return ans;
-    }
-
     public boolean isBalanced(TreeNode root) {
-        return isBal(root).first;
+         int x = withmaxhieght(root);
+
+         return x!=-1;
     }
 }
