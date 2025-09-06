7bee22cfc07909bd6b394582c0f90269bf50232c
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-09-07 00:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (29.98%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,28 +15,32 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
-        }
+        // if(p == null && q == null){
+        //     return true;
+        // }
 
-        if(p == null && q != null){
-            return false;
-        }
+        // if(p == null && q != null){
+        //     return false;
+        // }
 
-        if(p != null && q == null){
-            return false;
-        }
+        // if(p != null && q == null){
+        //     return false;
+        // }
 
-        boolean left = isSameTree(p.left,q.left);
-        boolean right = isSameTree(p.right, q.right);
+        // boolean left = isSameTree(p.left,q.left);
+        // boolean right = isSameTree(p.right, q.right);
 
-        boolean val = (p.val == q.val);
+        // boolean val = (p.val == q.val);
 
-        if(left && right && val){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // if(left && right && val){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+        if(p == null || q == null) return p == q;
+
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
     }
 }
