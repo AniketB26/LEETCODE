4d105cfae58681796df8f2fcbd931e9e75d9dd51
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-10-04 22:15:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (19.19%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -19,10 +19,20 @@ class Solution {
         return helper2(n-1) + helper2(n-2);
     }
     public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        // Arrays.fill(dp,-1);
         
 
-        return helper(n, dp);
+        // return helper(n, dp);
+
+        for(int i =2; i<= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
