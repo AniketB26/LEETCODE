982e9bb4e516e681880ad4e5d06cb19b0cc82276
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-10-13 19:59:45 +0530

    Time: 1 ms (99.96%) | Memory: 42.6 MB (83.15%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.java
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.java
@@ -1,14 +1,20 @@
 class Solution {
     public int sumDivisibleByK(int[] nums, int k) {
-        int[] fre = new int[101];
         int n = nums.length;
+        int len = 0;
+        for(int i =0; i<n; i++){
+            len = Math.max(len, nums[i]);
+        }
+        int[] fre = new int[len+1];
+        int m = fre.length;
+        
 
         for(int i =0; i<n; i++){
             fre[nums[i]]++;
         }
         int sum = 0;
 
-        for(int i =1; i<101; i++){
+        for(int i =1; i<m; i++){
             if(fre[i] == 0) continue;
 
             if(fre[i]%k == 0){
