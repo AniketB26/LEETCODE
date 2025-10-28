932bef571c39dec16afe8c4cf11b3c20030814df
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2025-10-28 11:22:52 +0530

    Time: 1 ms (98.76%) | Memory: 41.1 MB (77.58%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
@@ -1,16 +1,32 @@
 class Solution {
     public long removeZeros(long n) {
-        StringBuilder sb = new StringBuilder(String.valueOf(n));
+        // StringBuilder sb = new StringBuilder(String.valueOf(n));
 
-        for(int i =0; i< sb.length(); i++){
-            if(sb.charAt(i)=='0'){
-                sb.deleteCharAt(i);
-                i--;
+        // for(int i =0; i< sb.length(); i++){
+        //     if(sb.charAt(i)=='0'){
+        //         sb.deleteCharAt(i);
+        //         i--;
+        //     }
+        // }
+
+        // long nn = Long.parseLong(sb.toString());
+
+        // return nn;
+
+        long res = 0;
+        long pl = 1;
+
+        while(n>0){
+            long dig = n%10;
+
+            if(dig != 0){
+                res += dig * pl;
+                pl *=10;
             }
+            n = n/10;
+
         }
 
-        long nn = Long.parseLong(sb.toString());
-
-        return nn;
+        return res;
     }
 }
