bbfbc585de46cf11033b799f8a04732a4ec90d38
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-01 18:48:00 +0530

    Time: 29 ms (84.09%) | Memory: 149.7 MB (34.30%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         // List<Integer> list = new ArrayList<>();
-        int n = nums.length;
+        
         
         // for(int i=0; i< n-k+1 ; i++){
         //     int max = Integer.MIN_VALUE;
@@ -28,36 +28,68 @@ class Solution {
 
         // return res;
 
-        
-        int i =0, j =0;
-        Deque<Integer> dq = new LinkedList<>();
-        int[] res = new int[n-k+1];
+        int n = nums.length;
+        int r =0, l =0;
+
+        int[] ans = new int[n-k+1];
         int idx = 0;
 
-        while(j<n){
+        Deque<Integer> dq = new LinkedList<>();
 
-            while(!dq.isEmpty() && dq.peekLast() < nums[j]){
+        while(r<n){
+
+            while(!dq.isEmpty() && dq.peekLast() < nums[r]){
                 dq.pollLast();
             }
 
-            dq.offer(nums[j]);
+            dq.offer(nums[r]);
 
-            if(j-i+1 < k){
-                j++;
-            }
+            if(r-l+1 < k) r++;
 
-            else if(j-i+1 == k){
-                res[idx++] = dq.peekFirst();
+            else if(r-l+1 == k){
+                ans[idx++] = dq.peekFirst();
 
-                if(dq.peekFirst() == nums[i]){
+                if(dq.peekFirst() == nums[l]){
                     dq.pollFirst();
                 }
 
-                i++;
-                j++;
+                r++;
+                l++;
             }
         }
 
-        return res;
+        return ans;
+
     }
 }
+
+// int i =0, j =0;
+//         Deque<Integer> dq = new LinkedList<>();
+//         int[] res = new int[n-k+1];
+//         int idx = 0;
+
+//         while(j<n){
+
+//             while(!dq.isEmpty() && dq.peekLast() < nums[j]){
+//                 dq.pollLast();
+//             }
+
+//             dq.offer(nums[j]);
+
+//             if(j-i+1 < k){
+//                 j++;
+//             }
+
+//             else if(j-i+1 == k){
+//                 res[idx++] = dq.peekFirst();
+
+//                 if(dq.peekFirst() == nums[i]){
+//                     dq.pollFirst();
+//                 }
+
+//                 i++;
+//                 j++;
+//             }
+//         }
+
+//         return res;
