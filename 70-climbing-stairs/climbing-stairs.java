1d72c11d33cede3187defb5df96d6146e1f4eb5e
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-02 12:23:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int fib(int n){
-        if(n == 0) return 0;
+        if(n == 0) return 1;
         if(n == 1) return 1;
 
         return fib(n-1) + fib(n-2);
@@ -18,5 +18,7 @@ class Solution {
         }
 
         return dp[n];
+
+        // return fib(n);
     }
 }
