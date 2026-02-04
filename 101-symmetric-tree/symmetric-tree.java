8757516f5cf8dcc1b540a206128c278907ca34ab
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-04 10:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (12.87%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -21,8 +21,8 @@ class Solution {
     boolean helper(TreeNode left, TreeNode right){
         if(left == null || right ==  null) return left == right;
 
-        if(left.val != right.val) return false;
+        // if(left.val != right.val) return false;
 
-        return helper(left.left, right.right) && helper(left.right, right.left);
+        return (left.val == right.val) && helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
