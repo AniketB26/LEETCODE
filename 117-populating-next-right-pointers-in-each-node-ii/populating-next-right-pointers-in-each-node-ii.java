44de3831136cf59b4dd3869157636d9ebf27469f
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-05 10:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (50.50%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -23,29 +23,57 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if (root == null) return null;  
-        Queue<Node> q = new LinkedList<>();
-        // Queue<Node> childq = new LinkedList<>();
+        // if (root == null) return null;  
+        // Queue<Node> q = new LinkedList<>();
+        // // Queue<Node> childq = new LinkedList<>();
 
-        q.offer(root);
+        // q.offer(root);
 
-        while(!q.isEmpty()){
-            int size = q.size();
+        // while(!q.isEmpty()){
+        //     int size = q.size();
 
-            for(int i =0; i< size; i++){
+        //     for(int i =0; i< size; i++){
 
-                Node n = q.poll();
+        //         Node n = q.poll();
 
-                if(i < size -1){
-                    n.next = q.peek();
-                }
+        //         if(i < size -1){
+        //             n.next = q.peek();
+        //         }
 
-                if(n.left != null) q.offer(n.left);
-                if(n.right != null) q.offer(n.right);
-            }
+        //         if(n.left != null) q.offer(n.left);
+        //         if(n.right != null) q.offer(n.right);
+        //     }
 
             
 
+        // }
+
+        // return root;
+
+
+        Node curr = root;
+
+        while(curr != null){
+            Node dummy = new Node(0);
+            Node tail = dummy;
+
+
+            while(curr != null){
+
+                if(curr.left != null){
+                    tail.next = curr.left;
+                    tail = tail.next;
+                }
+
+                if(curr.right != null){
+                    tail.next = curr.right;
+                    tail = tail.next;
+                }
+                
+                curr = curr.next;
+            }
+            
+            curr = dummy.next;
         }
 
         return root;
