844a05e0572d11172549e531e969c557df9e79f2
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-07 16:45:55 +0530

    Time: 3 ms (99.65%) | Memory: 94.5 MB (66.88%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -12,29 +12,61 @@ class Solution {
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null) return true;
 
-        ListNode slow = head, fast = head;
+        // ListNode slow = head, fast = head;
+
+        // while(fast != null && fast.next != null){
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+
+        // ListNode prev = null , curr = slow;
+
+        // while(curr != null){
+        //     ListNode next = curr.next;
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+
+        // ListNode left = head, right = prev;
+
+        // while(right != null){
+        //     if(left.val != right.val) return false;
+        //     left = left.next;
+        //     right = right.next;
+        // }
+
+        // return true;
+
+
+        ListNode slow = head;
+
+        ListNode fast = head;
 
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        ListNode prev = null , curr = slow;
+        ListNode curr = slow, prev = null;
 
         while(curr != null){
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
+
         }
+
 
         ListNode left = head, right = prev;
 
         while(right != null){
-            if(left.val != right.val) return false;
+            if(left.val != right.val ) return false;
             left = left.next;
             right = right.next;
         }
+
 
         return true;
 
