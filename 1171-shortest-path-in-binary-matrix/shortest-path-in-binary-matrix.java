90cd75e2c11c4e56b7a6817e11ad7bde87d6ce9e
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-09 15:25:37 +0530

    Time: 13 ms (85.54%) | Memory: 47.9 MB (40.24%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int n = grid.length;
+
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
+            return -1;
+        }   
+        boolean[][] visited = new boolean[n][n];
+        q.offer(new int[]{0,0,1});
+
+        int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {-1,1}, {1,-1}, {-1,-1}};
+
+         
+        while(!q.isEmpty()){
+            int[] cell = q.poll();
+            int r = cell[0], c = cell[1] , dist = cell[2];
+
+            if(r == n-1 && c == n-1 ) return dist;
+
+
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+
+                if(nr >=0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0 ){
+                    grid[nr][nc] = 1;
+                    q.offer(new int[]{nr,nc, dist+1});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
