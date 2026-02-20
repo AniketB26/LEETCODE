d81fe45e3826eedc430a6eebd027472a54ae1356
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-02-20 19:00:17 +0530

    Time: 2 ms (91.20%) | Memory: 44.5 MB (15.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,43 +2,55 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         Queue<int[]> q = new LinkedList<>();
 
-        int m = grid.length, n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
         int fresh = 0;
 
-        for(int i=0; i<m ; i++){
-            for(int j =0; j<n; j++){
+        for(int i =0; i<grid.length; i++){
+            for(int j = 0; j<n; j++){
 
                 if(grid[i][j] == 2){
                     q.offer(new int[]{i,j,0});
                 }
-                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
             }
         }
 
+
         if(fresh == 0) return 0;
+
         int min =0;
 
-        int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        int[][] dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};
 
         while(!q.isEmpty()){
+
             int[] cell = q.poll();
+           
 
-            int r = cell[0], c = cell[1], time = cell[2];
-            min = time;
-
+            int r = cell[0], c = cell[1], t = cell[2];
+            min = t;
 
             for(int[] d : dir){
+
                 int nr = r + d[0];
                 int nc = c + d[1];
 
-
                 if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
+
                     grid[nr][nc] = 2;
                     fresh--;
-                    q.offer(new int[]{nr,nc, time+1});
+                    q.offer(new int[] {nr,nc,t+1});
                 }
+
             }
+
+            
+
+            
         }
 
         return fresh == 0 ? min : -1;
