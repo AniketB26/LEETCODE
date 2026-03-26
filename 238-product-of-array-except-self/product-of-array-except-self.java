c5f27e7aa7d56a146487c4b3469a379e0c0a8ece
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-03-26 21:08:27 +0530

    Time: 3 ms (26.52%) | Memory: 65.2 MB (69.61%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] arr = new int[nums.length];
+       int n = nums.length;
 
-        int[] preFix = new int[nums.length];
-        int[] sufFix = new int[nums.length];
-        preFix[0] = 1;
+       int[] pre = new int[n];
 
-         for(int i =1; i<nums.length; i++){
-            preFix[i] = preFix[i-1] * nums[i-1];
-         }
+       int[] suf = new int[n];
+       pre[0] = 1;
+       suf[n-1] = 1;
+
+       for(int i=1; i<n; i++){
+
+        pre[i] = pre[i-1]*nums[i-1];
+
         
-        sufFix[nums.length - 1] = 1;
+       }
 
-        for (int i = nums.length - 2; i >= 0; i--) {
-            sufFix[i] = sufFix[i + 1] * nums[i+1];
-        }
+       for(int i = n-2; i>=0; i--){
+        suf[i] = suf[i+1] * nums[i+1];
+       }
 
 
-        for(int i =0; i<nums.length; i++){
-            arr[i] = preFix[i] * sufFix[i];
-        }
+       int[] ans = new int[n];
 
-        return arr;
+       for(int i=0; i<n; i++){
+        ans[i] = pre[i] * suf[i];
+       }
+
+       return ans;
     }
 }
