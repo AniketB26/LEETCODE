4a69cdca4d79ecbcf82a111a66a350844ef7981a
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-05-04 22:11:46 +0530

    Time: 1 ms (70.99%) | Memory: 43.9 MB (13.97%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,53 +14,29 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode node, List<Integer> arr, int lvl){
-        if(node == null){
-            return;
-        }
-
-        if(lvl == arr.size()){
-            arr.add(node.val);
-        }
-
-        helper(node.right,arr,lvl+1);
-        helper(node.left,arr,lvl+1);
-    }
-
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        if(root == null){
-            return arr;
-        }
+        List<Integer> res = new ArrayList<>();
+
+        if(root == null) return res;
 
         Queue<TreeNode> q = new LinkedList<>();
-
-        q.offer(root);
-
+        q.add(root);
+        
         while(!q.isEmpty()){
-            int lvl = q.size();
 
-            for(int i =0; i< lvl; i++){
+            int size = q.size();
+
+            for(int i =0; i<size; i++){
+
                 TreeNode x = q.poll();
 
-                if(i== lvl-1){
-                    arr.add(x.val);
-                }
+                if(i == size-1) res.add(x.val);
 
-                if(x.left != null){
-                    q.offer(x.left);
-                }
-
-                if(x.right != null){
-                    q.offer(x.right);
-                }
+                if(x.left != null) q.add(x.left);
+                if(x.right != null) q.add(x.right);
             }
         }
 
-        return arr;
-
-        // List<Integer> arr = new ArrayList<>();
-        // helper(root, arr, 0);
-        // return arr;
+        return res;
     }
 }
