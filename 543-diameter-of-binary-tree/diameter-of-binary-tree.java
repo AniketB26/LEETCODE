3560e07fd6b1d6323c847cf87ebdb10f5188a9cc
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-05-20 18:12:22 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (25.56%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -16,21 +16,20 @@
 class Solution {
 
     int helper(TreeNode root, int[] maxi){
-        
-        if(root==null) return 0;
 
-        int left = helper(root.left,maxi);
-        int right = helper(root.right,maxi);
+        if(root == null) return 0;
+
+        int left = helper(root.left, maxi);
+        int right = helper(root.right, maxi);
 
         maxi[0] = Math.max(maxi[0], left+right);
 
-        return 1+ Math.max(left,right);
-
+        return Math.max(left,right) + 1;
     }
-
     public int diameterOfBinaryTree(TreeNode root) {
         int[] maxi = new int[1];
+
         helper(root,maxi);
-        return maxi[0];
+        return maxi[0]; 
     }
 }
