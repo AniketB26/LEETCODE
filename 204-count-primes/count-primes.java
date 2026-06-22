b9f51c712e7304e56315a3b4a34d0e07b98bc229
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-06-22 12:45:10 +0530

    Time: 105 ms (40.34%) | Memory: 51.4 MB (22.10%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,39 @@
+class Solution {
+    boolean checkPrime(int n){
+
+        for(int i =2; i*i <= n; i++){
+
+            if(n%i==0) return false;
+        }
+
+        return true;
+    }
+    public int countPrimes(int n) {
+        boolean[] seive = new boolean[n];
+
+        for(int i = 2; i<n; i++) seive[i] = true;
+
+        for(int i =2; i*i<n; i++){
+
+            if(seive[i]){
+
+                for(int j = i*i; j<n ; j+= i){
+
+                    seive[j] = false;
+                }
+            }
+        }
+
+        int cnt = 0;
+
+        for(int i =0; i<n ; i++){
+
+            if(seive[i]) cnt++;
+        }
+
+        return cnt;
+
+
+
+    }
+}
