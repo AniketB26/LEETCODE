14b9f6cdaba6a1e1db8694004718a7d6fb43f609
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-06-23 14:33:08 +0530

    Time: 1 ms (93.80%) | Memory: 43.1 MB (93.05%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,39 +1,27 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        // for(int i =0; i< numRows; i++){
-        //     List<Integer> ll = new ArrayList<>();
-           
-        //     for(int j =0; j <=i; j++){
-                
-        //        if(j == 0 || j == i){
-        //         ll.add(1);
-        //        }
-        //        else{
-        //         int val = list.get(i-1).get(j-1) + list.get(i-1).get(j);
-        //         ll.add(val);
-        //        }
-
-                
-        //     }
-
-        //     list.add(ll);
-        // }
+        List<List<Integer>> ans = new ArrayList<>();
 
 
-        for(int n = 0; n<numRows; n++){
-            List<Integer> row = new ArrayList<>();
-            long val = 1;
-            for(int k = 0; k<=n ; k++){
+        for(int i =0; i<numRows; i++){
+            List<Integer> list = new ArrayList<>();
 
-                row.add((int)val);
-                val = val*(n-k)/(k+1);
+            for(int j =0; j<=i ; j++){
+
+                if( j == 0 || j == i){
+                    list.add(1);
+                }
+                else{
+
+                    int val = ans.get(i-1).get(j-1) + ans.get(i-1).get(j);
+
+                    list.add(val);
+                }
             }
 
-            list.add(row);
+            ans.add(list);
         }
 
-        return list;
+        return ans;
     }
 }
