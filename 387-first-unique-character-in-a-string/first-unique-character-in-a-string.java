450adc9959b0b85b62b816d91464789bf3654edf
AniketB26 <139236533+AniketB26@users.noreply.github.com>
2026-07-30 02:55:20 +0530

    Time: 4 ms (99.72%) | Memory: 46.6 MB (93.51%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,49 +1,22 @@
 class Solution {
     public int firstUniqChar(String s) {
-    //     HashMap<Character,int[]> map = new HashMap<>();
-
-    //     char[] arr = s.toCharArray();
-
-    //     for(int i =0; i<arr.length; i++){
-    //         if(!map.containsKey(arr[i])){
-    //             map.put(arr[i], new int[]{1,i});
-    //         }
-    //         else{
-    //             map.get(arr[i])[0]++;
-    //         }
-    //     }
-
         
-
-    //     int min = Integer.MAX_VALUE;
-        
-    //     for(char x : map.keySet()){
-    //         int freq = map.get(x)[0];
-    //         int val = map.get(x)[1];
-    //         if(freq == 1){
-    //             min = Math.min(min,val);
-    //         }
-            
-            
-    //     }
-
-    //    return (min == Integer.MAX_VALUE) ? -1 : min;
+        int[] frq = new int[256];
 
         char[] arr = s.toCharArray();
-        int n = arr.length;
-        int[] freq = new int[256];
 
+        for(int i =0; i<arr.length; i++){
 
-        for(int i =0; i<n; i++){
-            freq[arr[i]]++;
+            frq[arr[i]]++;
         }
 
-        for(int i =0; i<n; i++){
-            if(freq[arr[i]] == 1) return i;
+
+        for(int i =0; i<arr.length; i++){
+
+            if(frq[arr[i]] == 1) return i;
         }
 
 
         return -1;
-
     }
 }
